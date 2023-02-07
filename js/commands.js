47ccc0fe401cb5efdade0c8eae334167ef5ed6ab
zvdy <78762257+zvdy@users.noreply.github.com>
2023-02-07 22:25:14 +0000

    app

---
+++ js/commands.js
@@ -0,0 +1,97 @@
+var password = "n0troot";
+var linkedin = "https://www.linkedin.com/in/cristian-bulzan/";
+var instagram = "https://www.instagram.com/cristxvn/";
+var github = "https://www.instagram.com/cristxvn/";
+var email = 'mailto:cristianblast@gmail.com';
+var leetcode = "https://leetcode.com/zvdy/";
+
+whois = [
+  "<br>",
+  "Hey, I'm Cristian aka Zvdy!👋",
+  "I'm a Software Developer and Cloud Expert,I build innovative software solutions and tools.👨‍💻",
+  "I'm interested in a lot of fields of techology like Cybersecurity, ML/AI and Backend development.💻",
+  "I work professionaly as Cloud Support for GCP products.⌨️",
+  "I help huge companies like NTT, Vodafone, J.P Morgan Bank and a lot of others.🌎",
+  "I'm currently based on Spain, Barcelona.🏠",
+  "I'm a huge fan of the Linux OS and I love to code in Python.🐧",
+  "I learned a lot being a self tought developer and I have some certifications like:", 
+  "         - Google Cloud Fundamentals",
+  "         - Google Crash Course on Python", 
+  "         - Google Cloud Fundamentals",
+  "         - Meta introduction to Databases for Back¡End Development",
+  "         - freeCodeCamp Scientific Computing with Python",
+  "         - And a lot of others, I'm always learning something new.📚",
+  "You can look into my certifications on my Linkedin",
+
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "“Who am I?” Are you even real? Are you a human being? Are you... In the matrix?.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/cristian-bulzan' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/cristxvn/' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/zvdy' + "</a>",
+  'leetcode       <a href="' + leetcode + '" target="_blank">leetcode/zvdy' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "All my project's are on my github.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Zvdy?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You just typed this',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Zady Terminal.</span>',
+"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+"         _nnnn_",
+"        dGGGGMMb",
+"       @p~qp~~qMb",
+"       M|@||@) M|",
+"      JS^[__/  qKL",
+"     dZP        qKRb",
+"    dZP          qKKb",
+"   fZP            SMMb",
+"   HZM            MMMM",
+"   FqM            MMMM",
+" __| '.        |\dS'qML",
+" |    `.       | `' \Zq            ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄▄▄▄  ▄▄   ▄▄ ",
+"_)      \.___.,|     .'          █       █  █ █  █      ██  █ █  █",
+"\____   )MMMMMP|   .'            █▄▄▄▄   █  █▄█  █  ▄    █  █▄█  █",
+"     `-'       `--'              ▄▄▄▄█  █       █ █ █   █       █",
+"                                █ ▄▄▄▄▄▄█       █ █▄█   █▄     ▄█",
+"                                █ █▄▄▄▄▄ █     ██       █ █   █  ",
+"                                █▄▄▄▄▄▄▄█ █▄▄▄█ █▄▄▄▄▄▄█  █▄▄▄█  ",
+"",
+"",
+"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
